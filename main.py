f59ff4cadd5a8f4207bf0fcd682340901a7b1b3b
Daniel Mikita <djm6907@psu.edu>
2020-09-08 14:27:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# Author: Daniel Mikita djm6907@psu.edu
+# Collaborator: Jiahui Lin jzl6335@psu.edu
+# Collaborator: Chen-Kuan Liao czl5899@psu.edu
+# Collaborator: Yeman Xu ybx5148@psu.edu
+# Section: 1
+# Breakout: 8
+
+def getLetterGrade(grade):
+
+  grade = float(input("Enter your CMPSC 131 grade: "))
+
+if grade >= 93.0:
+  letter = "A"
+elif grade >=90.0:
+  letter = "A-"
+elif grade >= 87.0:
+  letter = "B+"
+elif grade >= 83.0:
+  letter = "B"
+elif grade >= 80.0:
+  letter = "B-"
+elif grade >= 77.0:
+  letter = "C+"
+elif grade >= 70.0:
+  letter = "C"
+elif grade >= 60.0:
+  letter = "D"
+else:
+  letter = "F"
+
+print(f"Your letter grade for CMPSC 131 is  {letter}.")
