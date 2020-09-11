7136af2e76bdfe2b7139eedc8b3154a502803bf2
Daniel Mikita <djm6907@psu.edu>
2020-09-11 02:25:56 +0000

    formatting

--- main.py
+++ main.py
@@ -27,28 +27,10 @@ def getLetterGrade(grade):
 
 def run():
     ignore = input("Enter your CMPSC 131 grade: ")
+    ignore = float(ignore)
+    print(f"Your letter grade for CMPSC 131 is {getLetterGrade(ignore)}.")
+   
 
-grade = float(input("Enter your CMPSC 131 grade: "))
-if (grade >= 93.0):
-  letter = "A"
-elif (grade >=90.0) :
-  letter = "A-"
-elif (grade >=87.0) :
-  letter = "B+"
-elif (grade >=83.0) :
-  letter = "B"
-elif (grade >=80.0) :
-  letter = "B-"
-elif (grade >=73.0) :
-  letter = "C+"
-elif (grade >=70.0) :
-  letter = "C"
-elif (grade>= 60.0) :
-  letter = "D"
-else:
-  letter = "F"
-
-print(f"Your letter grade for CMPSC 131 is {letter}.")
-
-if __name__== "__main__":
+if __name__== "__main__" :
   run()
+ 
