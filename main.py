fc0eb941eb10c32430d4b7934f74cf9fef898c58
Daniel Mikita <djm6907@psu.edu>
2020-09-11 01:42:08 +0000

    subwindow fix

--- main.py
+++ main.py
@@ -5,28 +5,50 @@
 # Section: 1
 # Breakout: 8
 
-grade = float(input("Enter your CMPSC 131 grade: "))
+def getLetterGrade(grade):
+  if (grade >= 93.0):
+    return "A"
+  elif (grade >=90.0) :
+      return "A-"
+  elif (grade >=87.0) :
+    return "B+"
+  elif (grade >=83.0) :
+    return "B"
+  elif (grade >=80.0) :
+    return "B-"
+  elif (grade >=73.0) :
+    return "C+"
+  elif (grade >=70.0) :
+    return "C"
+  elif (grade>= 60.0) :
+    return "D"
+  else:
+    return "F"
 
-if grade >= 93.0:
- letter = "A"
-elif grade >=90.0:
+def run():
+    ignore = input("Enter your CMPSC 131 grade: ")
+
+grade = float(input("Enter your CMPSC 131 grade: "))
+if (grade >= 93.0):
+  letter = "A"
+elif (grade >=90.0) :
   letter = "A-"
-elif grade >= 87.0:
+elif (grade >=87.0) :
   letter = "B+"
-elif grade >= 83.0:
+elif (grade >=83.0) :
   letter = "B"
-elif grade >= 80.0:
- letter = "B-"
-elif grade >= 77.0:
+elif (grade >=80.0) :
+  letter = "B-"
+elif (grade >=73.0) :
   letter = "C+"
-elif grade >= 70.0:
+elif (grade >=70.0) :
   letter = "C"
-elif grade >= 60.0:
+elif (grade>= 60.0) :
   letter = "D"
 else:
   letter = "F"
 
-print(f"Your letter grade for CMPSC 131 is  {letter}.")
+print(f"Your letter grade for CMPSC 131 is {letter}.")
 
-#if_name_== "_main_"
-#run()
+if __name__== "__main__":
+  run()
